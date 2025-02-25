9b04e4baa5592af0a6a815a02dac3849373e403d
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-02-25 19:34:10 +0000

    c

--- src/components/PokedexApp.jsx
+++ src/components/PokedexApp.jsx
@@ -19,10 +19,11 @@ const regions = [
 	{ name: "Sinnoh", start: 387, end: 493 },
 	{ name: "Unova", start: 494, end: 649 },
 	{ name: "Kalos", start: 650, end: 721 },
-	{ name: "Alola", start: 722, end: 809 },
+	{ name: "Alola", start: 722, end: 807 },
 	{ name: "Galar", start: 810, end: 898 },
 	{ name: "Hisui", start: 899, end: 905 },
 	{ name: "Paldea", start: 906, end: 1025 },
+	{ name: "Unknown", start: 808, end: 809 },
 ];
 
 /*     

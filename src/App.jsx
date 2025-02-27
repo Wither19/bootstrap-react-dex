00af84a247ef4beeee820447819b781fbfda838f
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-02-27 15:09:41 +0000

    c

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,13 @@ function App() {
 					<div className="col-4 d-flex justify-content-center">
 						<div className="display-4 pokedex-title">Pokédex</div>
 					</div>
-					<div className="col-4"></div>
+					<div className="col-4">
+						<div className="icon grass">
+							<img
+								src={`https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/5781623f147f1bf850f426cfe1874ba56a9b75ee/icons/grass.svg`}
+							/>
+						</div>
+					</div>
 				</div>
 				<PokedexApp />
 			</div>

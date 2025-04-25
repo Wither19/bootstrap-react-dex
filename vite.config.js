68d9bed435f0e95d6c6e3be4bc586b6e86fc318b
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-04-25 12:14:50 +0000

    c

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/bootstrap-react-dex/",
 	resolve: {
 		alias: {
 			"~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),

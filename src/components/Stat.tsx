ea23409691056e35f83af84344af60cd2802b732
Joseph Vranicar <josephvranicar1@gmail.com>
2025-04-26 10:43:47 -0400

    c

--- src/components/Stat.tsx
+++ src/components/Stat.tsx
@@ -22,34 +22,17 @@ const getStatName = (stat: string) => {
 	return n;
 };
 
-// function iterateComparison<T>(
-// 	arr: Array<number>,
-// 	val: number,
-// 	returnArray: Array<T>
-// ): T | undefined {
-// 	let i = 0;
-// 	var returnValue: T | undefined = undefined;
-// 	for (const a of arr) {
-// 		// You have reached the end of the array and would normally attempt to iterate out of bounds.
-// 		if (i + 1 >= arr.length) {
-// 			returnValue = returnArray[arr.length - 1];
-// 		}
-// 		// Value before iteration.
-// 		else if (i == 0) {
-// 			returnValue = returnArray[i];
-// 		}
-// 		// Normal comparison.
-// 		else if (val > a && val <= a) {
-// 			returnValue = returnArray[i];
-// 		} else {
-// 			return undefined;
-// 		}
-// 		i++;
-// 	}
-// 	return returnValue;
-// }
-
 function Stat({ name, value }: { name: string; value: number }) {
+	const findStat = (): number => {
+		let colorIndex: number = 0;
+		for (let s of statThresholds) {
+			if (value > s) {
+				colorIndex += 1;
+			}
+		}
+		return colorIndex;
+	};
+
 	return (
 		<div className="stat-title">
 			{getStatName(name)}
@@ -57,12 +40,11 @@ function Stat({ name, value }: { name: string; value: number }) {
 				className="stat-bar"
 				style={{
 					width: (value / 255) * 500,
-					backgroundColor: "red",
+					backgroundColor: statColors[findStat()],
 				}}>
 				{value == 0 ? "Loading..." : value}
 			</div>
 		</div>
 	);
 }
-
 export default Stat;

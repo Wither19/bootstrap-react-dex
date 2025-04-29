71715adfd815d26c032229f7c788f1c0e9ce9cb6
Joseph Vranicar <josephvranicar1@gmail.com>
2025-04-29 09:10:06 -0400

    c

--- src/components/PokedexItem.tsx
+++ src/components/PokedexItem.tsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-
-import axios from "axios";
+import { useState } from "react";
 
 import capitalize from "capitalize";
 
@@ -10,36 +8,46 @@ import { motion } from "motion/react";
 
 // Adds leading zeroes to a number until it is four digits long
 
-const leadingZeroes = (num, size) => {
-  return num.toString().padStart(size, 0);
+const leadingZeroes = (num: number, size: number) => {
+	return num.toString().padStart(size, "0");
 };
 
-function PokedexItem(props) {
-  const [pkmn, setPkmn] = useState({});
-  const [itemShiny, setShinyState] = useState(false);
+type PokedexItemProps = {
+	num: number;
+	name: string;
+	selected: boolean;
+	click: (event: React.MouseEvent<HTMLDivElement>) => void;
+};
 
-  return (
-    <>
-      <motion.div
-        key={props.name}
-        className={`list-group-item ${props.selected ? "active" : ""} pokemon-list-item`}
-        onClick={props.click}
-      >
-        <motion.img
-          src={`https://img.pokemondb.net/sprites/scarlet-violet/icon/avif/${props.name}.avif`}
-          className="sprite"
-          whileHover={{
-            scale: 1.1,
-            transition: { duration: 0.55 },
-          }}
-        />
-        <div className="card-body">
-          <b>#{leadingZeroes(props.num, 4)}</b> -{" "}
-          <span className="card-title text-nowrap">{capitalize(props.name.replace("-", " "))}</span>
-        </div>
-      </motion.div>
-    </>
-  );
+function PokedexItem({ num, name, selected, click }: PokedexItemProps) {
+	const [pkmn, setPkmn] = useState({});
+	const [itemShiny, setShinyState] = useState(false);
+
+	return (
+		<>
+			<motion.div
+				key={name}
+				className={`list-group-item ${
+					selected ? "active" : ""
+				} pokemon-list-item`}
+				onClick={click}>
+				<motion.img
+					src={`https://img.pokemondb.net/sprites/scarlet-violet/icon/avif/${name}.avif`}
+					className="sprite"
+					whileHover={{
+						scale: 1.1,
+						transition: { duration: 0.55 },
+					}}
+				/>
+				<div className="card-body">
+					<b>#{leadingZeroes(num, 4)}</b> -{" "}
+					<span className="card-title text-nowrap">
+						{capitalize(name.replace("-", " "))}
+					</span>
+				</div>
+			</motion.div>
+		</>
+	);
 }
 
 export default PokedexItem;

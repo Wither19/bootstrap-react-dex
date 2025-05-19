5af7b0b7c611e73cbaefa7bf0f126d90536cc7c8
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-05-19 18:18:44 +0000

    c

--- src/components/PkmnSearchBar.tsx
+++ src/components/PkmnSearchBar.tsx
@@ -1,7 +1,5 @@
 import TextField from "@mui/material/TextField";
 
-import { Search } from "lucide-react";
-
 type PkmnSearchBarProps = {
 	typing: (event: React.KeyboardEvent<HTMLInputElement>) => void;
 };
@@ -12,15 +10,6 @@ function PkmnSearchBar({ typing }: PkmnSearchBarProps) {
 			label="Search for Pokémon..."
 			variant="standard"
 			onKeyUp={typing}
-			slotProps={{
-				input: {
-					startAdornment: (
-						<div style={{ margin: "4px 5px 0px 5px" }}>
-							<Search />
-						</div>
-					),
-				},
-			}}
 		/>
 	);
 }

d2453b024b94dfc4d15e695b51d4099a495182cb
Wither19 <146484561+Wither19@users.noreply.github.com>
2025-05-21 18:36:35 +0000

    c

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { createTheme, ThemeProvider } from "@mui/material";
+
 import "./scss/App.scss";
+
+// Roboto fallback for Material UI
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/400-italic.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/700-italic.css";
 
 import "@fontsource/ubuntu-sans/400.css";
 import "@fontsource/ubuntu-sans/400-italic.css";
 import "@fontsource/ubuntu-sans/700.css";
 import "@fontsource/ubuntu-sans/700-italic.css";
-
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/400-italic.css";
-import "@fontsource/roboto/700.css";
-import "@fontsource/roboto/700-italic.css";
 
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/400-italic.css";
@@ -20,8 +23,16 @@ import "@fontsource/inter/700-italic.css";
 
 import App from "./App.js";
 
+const theme = createTheme({
+	typography: {
+		fontFamily: '"Ubuntu Sans", "Roboto", "Arial", "sans-serif"',
+	},
+});
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</StrictMode>
 );
